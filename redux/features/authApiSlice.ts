2abad8d1e141c69d4343b108e2e4b84a4732917a
ColinAdo <colinadodev30@gmail.com>
2024-09-11 14:52:53 +0300

    Add auth endpoints

---
+++ redux/features/authApiSlice.ts
@@ -0,0 +1,96 @@
+import { apiSlice } from "../services/apiSlice";
+
+interface User {
+  username: string;
+  email: string;
+}
+
+interface SocialAuthArgs {
+  provider: string;
+  state: string;
+  code: string;
+}
+
+interface CreateUserResponse {
+  success: boolean;
+  user: User;
+}
+
+const authApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    retrieveUser: builder.query<User, void>({
+      query: () => "/users/me/",
+    }),
+    socialAuthenticate: builder.mutation<CreateUserResponse, SocialAuthArgs>({
+      query: ({ provider, state, code }) => ({
+        url: `/o/${provider}/?state=${encodeURIComponent(
+          state
+        )}&code=${encodeURIComponent(code)}`,
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }),
+    }),
+    register: builder.mutation({
+      query: ({ username, email, password, re_password }) => ({
+        url: "/users/",
+        method: "POST",
+        body: { username, email, password, re_password },
+      }),
+    }),
+    login: builder.mutation({
+      query: ({ email, password }) => ({
+        url: "/jwt/create/",
+        method: "POST",
+        body: { email, password },
+      }),
+    }),
+    verify: builder.mutation({
+      query: () => ({
+        url: "/jwt/verify/",
+        method: "POST",
+      }),
+    }),
+    activation: builder.mutation({
+      query: ({ uid, token }) => ({
+        url: "/users/activation/",
+        method: "POST",
+        body: { uid, token },
+      }),
+    }),
+    logout: builder.mutation({
+      query: () => ({
+        url: "/logout/",
+        method: "POST",
+      }),
+    }),
+    passwordRest: builder.mutation({
+      query: (email) => ({
+        url: "/users/reset_password/",
+        method: "POST",
+        body: { email },
+      }),
+    }),
+    passwordRestConfirm: builder.mutation({
+      query: ({ uid, token, new_password, re_new_password }) => ({
+        url: "/users/reset_password_confirm/",
+        method: "POST",
+        body: { uid, token, new_password, re_new_password },
+      }),
+    }),
+  }),
+});
+
+export const {
+  useRetrieveUserQuery,
+  useSocialAuthenticateMutation,
+  useRegisterMutation,
+  useLoginMutation,
+  useVerifyMutation,
+  useActivationMutation,
+  useLogoutMutation,
+  usePasswordRestMutation,
+  usePasswordRestConfirmMutation,
+} = authApiSlice;
