040bd4533bad2487b3472a9f8ef31fe249715184
ColinAdo <colinadodev30@gmail.com>
2024-09-11 14:26:40 +0300

    Add QRT query for reauth

--- redux/store.ts
+++ redux/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/authSlice";
+import { apiSlice } from "./services/apiSlice";
 
 export const store = configureStore({
   reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
     auth: authReducer,
   },
   devTools: process.env.NODE_ENV !== "production",
