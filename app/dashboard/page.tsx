42ed93b0414c8729e86dbfe281aa665f36f6028a
ColinAdo <colinadodev30@gmail.com>
2024-09-12 10:51:27 +0300

    Add dashboard wth user list

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,43 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { List, Spinner } from "@/components/common";
+import { useRetrieveUserQuery } from "@/redux/features/authApiSlice";
+
 export default function Page() {
-  return <h2>Dashboard page</h2>;
+  const router = useRouter();
+  const { data: user, isLoading } = useRetrieveUserQuery();
+  const config = [
+    {
+      label: "Username",
+      value: user?.username,
+    },
+    {
+      label: "Email",
+      value: user?.email,
+    },
+  ];
+
+  if (isLoading) {
+    return (
+      <div className="flex justify-center my-8">
+        <Spinner lg />
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <header className="bg-white shadow">
+        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+            Dashboard
+          </h1>
+        </div>
+      </header>
+      <main className="mx-auto max-w-7xl py-6 my-8 sm:px-6 lg:px-8">
+        <List config={config} />
+      </main>
+    </>
+  );
 }
--- components/common/index.ts
+++ components/common/index.ts
@@ -2,3 +2,4 @@ export { default as Navbar } from "./Navbar";
 export { default as Footer } from "./Footer";
 export { default as Spinner } from "./Spinner";
 export { default as NavLink } from "./NavLink";
+export { default as List } from "./List";
