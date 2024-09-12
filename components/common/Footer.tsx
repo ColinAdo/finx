46b1f85e24c3dd49fe372a253d3ac2e4c74e130d
ColinAdo <colinadodev30@gmail.com>
2024-09-12 08:59:15 +0300

    Add styled footer

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -1,3 +1,13 @@
 export default function Footer() {
-  return <h2>Footer </h2>;
+  return (
+    <footer className="bg-gray-100 h-16">
+      <div className="h-full px-2">
+        <div className="flex items-center justify-center h-full">
+          <p className="text-gray-500 text-xs">
+            &copy; 2024 Monie, Inc. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
 }
