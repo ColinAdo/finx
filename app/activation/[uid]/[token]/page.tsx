fc762c5ec3171a98fe83a3e006a75670710397c1
ColinAdo <colinadodev30@gmail.com>
2024-09-12 07:21:09 +0300

    Add account activation

--- app/activation/[uid]/[token]/page.tsx
+++ app/activation/[uid]/[token]/page.tsx
@@ -1,3 +1,42 @@
-export default function Page() {
-  return <h2>Activating account page</h2>;
+"use client";
+
+import { useEffect } from "react";
+import { useActivationMutation } from "@/redux/features/authApiSlice";
+import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
+
+interface Props {
+  params: {
+    uid: string;
+    token: string;
+  };
+}
+
+export default function Page({ params }: Props) {
+  const router = useRouter();
+  const [activation] = useActivationMutation();
+
+  useEffect(() => {
+    const { uid, token } = params;
+    activation({ uid, token })
+      .unwrap()
+      .then(() => {
+        toast.success("Account activated successfully");
+      })
+      .catch(() => {
+        toast.error("Failed to activate account");
+      })
+      .finally(() => {
+        router.push("/auth/login");
+      });
+  }, []);
+  return (
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Activiating your account...
+        </h2>
+      </div>
+    </div>
+  );
 }
