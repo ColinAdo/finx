152d054f2fae4f4c1df3f0247b54c984c500784a
ColinAdo <colinadodev30@gmail.com>
2024-09-13 16:41:45 +0300

    Add image configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "8000",
+        pathname: "/media/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
