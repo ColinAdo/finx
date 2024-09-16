95da09bd2447b900c5c92ba5d11728c0ffdd64c3
ColinAdo <colinadodev30@gmail.com>
2024-09-16 14:45:01 +0300

    Add proper resgister form

---
+++ components/forms/ReuseRegisterForm.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { ReuseForm } from "@/components/forms";
+import { useRegister } from "@/hooks";
+import { RegisterSchema } from "@/lib/schemas";
+
+export default function ReuseRegisterForm() {
+  const {
+    username,
+    email,
+    password,
+    re_password,
+    isLoading,
+    onChange,
+    onSubmit,
+  } = useRegister();
+  const form = useForm<z.infer<typeof RegisterSchema>>({
+    resolver: zodResolver(RegisterSchema),
+    defaultValues: {
+      username: username,
+      email: email,
+      password: password,
+      re_password: re_password,
+    },
+  });
+
+  const config = [
+    {
+      control: form.control,
+      name: "username",
+      formLabel: "Username",
+      placeholder: "Enter your username",
+      type: "text",
+    },
+    {
+      control: form.control,
+      name: "email",
+      formLabel: "Email address",
+      placeholder: "Enter your email address",
+      type: "email",
+    },
+    {
+      control: form.control,
+      name: "password",
+      formLabel: "Password",
+      placeholder: "Enter your password",
+      type: "password",
+    },
+    {
+      control: form.control,
+      name: "re_password",
+      formLabel: "Confirm password",
+      placeholder: "Enter your username",
+      type: "password",
+    },
+  ];
+
+  return (
+    <ReuseForm
+      config={config}
+      form={form}
+      schema={RegisterSchema}
+      onSubmit={onSubmit}
+      btnText="Submit"
+      isLoading={isLoading}
+    />
+  );
+}
