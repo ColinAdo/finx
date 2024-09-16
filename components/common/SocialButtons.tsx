48aceafa1bb0369a1faa182f8fc1169124d7fa98
ColinAdo <colinadodev30@gmail.com>
2024-09-16 10:51:49 +0300

    Add proper social buttons

--- components/common/SocialButtons.tsx
+++ components/common/SocialButtons.tsx
@@ -8,11 +8,9 @@ export default function SocialButtons() {
   return (
     <div className="flex justify-between items-center gap-2 mt-5">
       <SocialButton provider="google" onClick={continueWithGoogle}>
-        Signin with
         <ImGoogle className="ml-3" />
       </SocialButton>
       <SocialButton provider="github" onClick={continueWithGithub}>
-        Signin with
         <ImGithub className="ml-3" />
       </SocialButton>
     </div>
