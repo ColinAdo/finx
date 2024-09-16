9c4248082feb60c26af8e9385ea587a4ae975c3f
ColinAdo <colinadodev30@gmail.com>
2024-09-16 08:01:30 +0300

    Add create post page

--- app/dashboard/create/page.tsx
+++ app/dashboard/create/page.tsx
@@ -1,191 +1,27 @@
 "use client";
 
-import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-} from "@/components/ui/dialog";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Button } from "@/components/ui/button";
-import { AspectRatio } from "@/components/ui/aspect-ratio";
 import { usePathname, useRouter } from "next/navigation";
-import { useState } from "react";
-import { useMount } from "@/hooks";
-import { Input } from "@/components/ui/input";
-import { CreatePost } from "@/lib/schemas";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { CameraIcon } from "lucide-react";
-import { useForm } from "react-hook-form";
-import { toast } from "sonner";
-import { z } from "zod";
-import { useEdgeStore } from "@/lib/edgestore";
-import Image from "next/image";
+import { FormDialog } from "@/components/common";
+import { CreatePostForm } from "@/components/forms";
 
 export default function Page() {
-  const [file, setFile] = useState<File>();
-  const [progress, setProgress] = useState(0);
-  const [fileUrl, setfileUrl] = useState<{
-    url: string;
-  }>();
-  const { edgestore } = useEdgeStore();
-
-  const pathname = usePathname();
-  const isCreatePage = pathname === "/dashboard/create";
   const router = useRouter();
-  const mount = useMount();
-  const form = useForm<z.infer<typeof CreatePost>>({
-    resolver: zodResolver(CreatePost),
-    defaultValues: {
-      caption: "",
-    },
-  });
+  const pathname = usePathname();
 
-  if (!mount) return null;
-
-  const handleSubmit = (data: z.infer<typeof CreatePost>) => {
-    router.push("/");
+  const isCreatePage = pathname === "/dashboard/create";
+  const onOpenChange = (open = isCreatePage) => {
+    !open && router.back();
   };
 
   return (
     <div>
-      <Dialog
-        open={isCreatePage}
-        onOpenChange={(open) => !open && router.back()}
+      <FormDialog
+        requiredRoute={isCreatePage}
+        onOpenChange={onOpenChange}
+        dialogTitle="Create new post"
       >
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Create new post</DialogTitle>
-          </DialogHeader>
-
-          <Form {...form}>
-            <form
-              onSubmit={form.handleSubmit(handleSubmit)}
-              className="space-y-4"
-            >
-              {fileUrl ? (
-                <div className="h-80 md:h-[300px] overflow-hidden rounded-md">
-                  <AspectRatio ratio={1 / 1} className="relative h-full">
-                    {fileUrl?.url.endsWith(".mp4") ? (
-                      <video
-                        controls
-                        className="rounded-md object-cover"
-                        width="100%"
-                        height="auto"
-                      >
-                        <source src={fileUrl?.url} type="video/mp4" />
-                        Your browser does not support the video tag.
-                      </video>
-                    ) : (
-                      <Image
-                        src={fileUrl?.url}
-                        alt="Post preview"
-                        fill
-                        className="rounded-md object-cover"
-                      />
-                    )}
-                  </AspectRatio>
-                </div>
-              ) : (
-                <FormField
-                  control={form.control}
-                  name="fileUrl"
-                  render={({ field, fieldState }) => (
-                    <FormItem>
-                      <FormControl>
-                        <>
-                          <label
-                            htmlFor="fileInput"
-                            className="flex justify-center cursor-pointer max-w-full p-3"
-                          >
-                            <CameraIcon className="flex justify-center items-center dark:text-gray-200 text-gray-900" />
-                            <input
-                              id="fileInput"
-                              type="file"
-                              className="hidden"
-                              onChange={(e) => {
-                                setFile(e.target.files?.[0]);
-                              }}
-                            />
-                          </label>
-
-                          <div className="h-[6px] max-w-full border rounded overflow-hidden">
-                            <div
-                              className="h-full bg-white transition-all duration-150"
-                              style={{
-                                width: `${progress}%`,
-                              }}
-                            />
-                          </div>
-                        </>
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              )}
-
-              {!!fileUrl && (
-                <FormField
-                  control={form.control}
-                  name="caption"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel htmlFor="caption">Caption</FormLabel>
-                      <FormControl>
-                        <Input
-                          type="caption"
-                          id="caption"
-                          placeholder="Write a caption..."
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              )}
-
-              <div className="flex justify-between">
-                <Button
-                  type="submit"
-                  className="flex justify-center"
-                  disabled={!!fileUrl}
-                  onClick={async () => {
-                    if (file) {
-                      const res = await edgestore.publicFiles.upload({
-                        file,
-                        onProgressChange: (progress) => {
-                          setProgress(progress);
-                        },
-                      });
-                      setfileUrl({
-                        url: res.url,
-                      });
-                    }
-                  }}
-                >
-                  Next
-                </Button>
-                <Button
-                  type="submit"
-                  className="flex justify-center"
-                  disabled={!fileUrl}
-                >
-                  Post
-                </Button>
-              </div>
-            </form>
-          </Form>
-        </DialogContent>
-      </Dialog>
+        <CreatePostForm />
+      </FormDialog>
     </div>
   );
 }
