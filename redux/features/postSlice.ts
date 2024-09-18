e123b5e8f675bba3e1408313c9170c8805cb27f7
ColinAdo <colinadodev30@gmail.com>
2024-09-18 15:28:10 +0300

    Add posts slice

--- redux/features/postSlice.ts
+++ redux/features/postSlice.ts
@@ -1,5 +1,39 @@
 import { apiSlice } from "../services/apiSlice";
 
+interface User {
+  id: number;
+  username: string;
+  profile_picture: string;
+}
+
+interface Comment {
+  id: number;
+  owner: User;
+  post: number;
+  comment: string | null;
+  comment_image?: string | null;
+  created_at: Date;
+}
+
+interface Like {
+  id: number;
+  user: User;
+  post: number;
+  created_at: Date | null;
+}
+
+interface Post {
+  id: number;
+  author: User;
+  fileUrl: string;
+  caption: string | null;
+  created_at: Date;
+  comments: Comment[];
+  comments_count: number;
+  likes: Like[];
+  likes_count: number;
+}
+
 const postSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     createPost: builder.mutation({
@@ -9,7 +43,22 @@ const postSlice = apiSlice.injectEndpoints({
         body: { caption, fileUrl },
       }),
     }),
+    retrievePost: builder.query<Post[], void>({
+      query: () => ({
+        url: "/posts/",
+      }),
+    }),
+    deletePost: builder.mutation<void, number>({
+      query: (postId) => ({
+        url: `/posts/${postId}/`,
+        method: "DELETE",
+      }),
+    }),
   }),
 });
 
-export const { useCreatePostMutation } = postSlice;
+export const {
+  useCreatePostMutation,
+  useRetrievePostQuery,
+  useDeletePostMutation,
+} = postSlice;
