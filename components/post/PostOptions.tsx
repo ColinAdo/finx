a5273e154a8c71a0e09956ea2b5df66f99e70440
ColinAdo <colinadodev30@gmail.com>
2024-09-18 18:00:23 +0300

    Add post action dialog

---
+++ components/post/PostOptions.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import { DeletePost } from "@/lib/schemas";
+import { cn } from "@/lib/utils";
+import { FormEvent, useState } from "react";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { SubmitButton } from "@/components/post";
+import { MoreHorizontal } from "lucide-react";
+import Link from "next/link";
+import { toast } from "sonner";
+import { useRetrieveUserQuery } from "@/redux/features/authApiSlice";
+import {
+  useDeletePostMutation,
+  useGetPostQuery,
+} from "@/redux/features/postSlice";
+
+interface User {
+  id: number;
+  username: string;
+  profile_picture: string;
+}
+
+interface Post {
+  id: number;
+  author: User;
+}
+
+interface Props {
+  post: Post;
+  userId?: number;
+  className?: string;
+}
+
+export default function PostOptions({ post, className }: Props) {
+  const [deletePost] = useDeletePostMutation();
+  const { refetch } = useGetPostQuery();
+
+  const { data: user } = useRetrieveUserQuery();
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const isPostMine = post.author.id === user?.id;
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      await deletePost(post.id).unwrap();
+      refetch();
+      toast.success("Post deleted successfully");
+      setIsDialogOpen(false);
+    } catch (error) {
+      toast.error("Error deleting post");
+      console.error("Error deleting post:", error);
+    }
+  };
+
+  return (
+    <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+      <DialogTrigger asChild>
+        <MoreHorizontal
+          className={cn(
+            "h-5 w-5 cursor-pointer dark:text-neutral-400",
+            className
+          )}
+        />
+      </DialogTrigger>
+      <DialogContent className="dialogContent">
+        {isPostMine && (
+          <form onSubmit={handleSubmit} className="postOption">
+            <input type="hidden" name="id" value={post.id} />
+            <SubmitButton className="text-red-500 font-bold disabled:cursor-not-allowed w-full p-3">
+              Delete post
+            </SubmitButton>
+          </form>
+        )}
+
+        {isPostMine && (
+          <Link
+            scroll={false}
+            href={`/dashboard/p/${post.id}/edit`}
+            className="postOption p-3"
+          >
+            Edit
+          </Link>
+        )}
+
+        <form action="" className="postOption border-0">
+          <button className="w-full p-3">Hide like count</button>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+}
