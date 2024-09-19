2c21edd708b3faac443a4e4f0bbd997b3198da9d
ColinAdo <colinadodev30@gmail.com>
2024-09-19 21:36:30 +0300

    Add post in suspense

--- app/dashboard/(home)/page.tsx
+++ app/dashboard/(home)/page.tsx
@@ -1,41 +1,14 @@
-"use client";
-
-import { List, Spinner } from "@/components/common";
-import { useRetrieveUserQuery } from "@/redux/features/authApiSlice";
+import { Suspense } from "react";
+import { Posts } from "@/components/post";
 
 export default function Page() {
-  const { data: user, isLoading } = useRetrieveUserQuery();
-  const config = [
-    {
-      label: "Username",
-      value: user?.username,
-    },
-    {
-      label: "Email",
-      value: user?.email,
-    },
-  ];
-
-  if (isLoading) {
-    return (
-      <div className="flex justify-center my-8">
-        <Spinner lg />
-      </div>
-    );
-  }
-
   return (
-    <>
-      <header className="bg-white shadow">
-        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold tracking-tight text-gray-900">
-            Dashboard
-          </h1>
-        </div>
-      </header>
-      <main className="mx-auto max-w-7xl py-6 my-8 sm:px-6 lg:px-8">
-        <List config={config} />
-      </main>
-    </>
+    <main className="flex w-full flex-grow">
+      <div className="flex flex-col flex-1 gap-y-8 max-w-lg mx-auto pb-20">
+        <Suspense>
+          <Posts />
+        </Suspense>
+      </div>
+    </main>
   );
 }
