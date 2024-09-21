baafcd69965d281856b143d4abb8686953ddd049
ColinAdo <colinadodev30@gmail.com>
2024-09-21 13:35:26 +0300

    Add persistent like icon

--- components/post/BookmarkButton.tsx
+++ components/post/BookmarkButton.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { FormEvent, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { ActionIcons } from "@/components/post";
 import { Bookmark } from "lucide-react";
 import { toast } from "sonner";
@@ -30,10 +30,15 @@ export default function BookmarkButton({ post }: { post: Post }) {
   const { refetch } = useGetPostQuery();
   const { data } = useRetrieveUserQuery();
 
-  const initialbookmarked = post.bookmarks.some(
+  const initialBookmark = post.bookmarks.some(
     (bookmark) => bookmark.user.id === data?.id
   );
-  const [bookmarked, setBookmarked] = useState(initialbookmarked);
+
+  const [bookmarked, setBookmarked] = useState(
+    localStorage.getItem(`${post.id}-bookmarked`) === "true"
+      ? true
+      : initialBookmark
+  );
 
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -42,15 +47,19 @@ export default function BookmarkButton({ post }: { post: Post }) {
       await bookmarkPost(post.id).unwrap();
       refetch();
       setBookmarked((prevLiked) => !prevLiked);
+      localStorage.setItem(`${post.id}-bookmarked`, String(bookmarked));
     } catch (error) {
       toast.error("Error bookmarking post");
     }
   };
 
+  useEffect(() => {
+    localStorage.setItem(`${post.id}-bookmarked`, String(bookmarked));
+  }, [bookmarked, post.id]);
+
   return (
     <form onSubmit={handleSubmit} className="ml-auto">
       <input type="hidden" name="post" value={post.id} />
-
       <ActionIcons>
         <Bookmark
           className={cn("h-6 w-6", {
