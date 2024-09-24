e16e834367dee3898fde00ce0dc462ddbdcb0343
ColinAdo <colinadodev30@gmail.com>
2024-09-24 18:40:43 +0300

    Add proper styling to edit post input field

--- components/forms/EditPostForm.tsx
+++ components/forms/EditPostForm.tsx
@@ -88,7 +88,7 @@ export default function EditPostForm({ post }: Props) {
           value={caption}
           onChange={(e) => setCaption(e.target.value)}
           placeholder="Write a caption..."
-          className="mt-1 block w-full hover:border-none rounded-md border-gray-300 shadow-sm focus:border-black dark:bg-black text-black dark:text-white focus:ring dark:focus:ring-white "
+          className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
         />
       </div>
 
