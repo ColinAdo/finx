39f9258c96f6d1dd163a81228211995eaf49ac08
ColinAdo <colinadodev30@gmail.com>
2024-09-24 13:25:44 +0300

    Add styled 404-page notfound

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,28 +1,29 @@
-import Link from "next/link";
+"use client";
+
+import { useRouter } from "next/navigation";
+import { Frown } from "lucide-react";
 
 export default function NotFound() {
+  const router = useRouter();
+
+  const handleGoBack = () => {
+    router.back();
+  };
+
   return (
-    <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-      <div className="text-center">
-        <p className="text-base font-semibold text-indigo-600">404</p>
-        <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-          Page not found
-        </h1>
-        <p className="mt-6 text-base leading-7 text-gray-600">
-          Sorry, we couldn&apos;t find the page you&apos;re looking for.
-        </p>
-        <div className="mt-10 flex items-center justify-center gap-x-6">
-          <Link
-            href="/"
-            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Go back home
-          </Link>
-          <Link href="/" className="text-sm font-semibold text-gray-900">
-            Contact support <span aria-hidden="true">&rarr;</span>
-          </Link>
-        </div>
-      </div>
+    <main className="flex h-full flex-col py-52 items-center justify-center gap-2">
+      <Frown className="w-10 text-gray-400" />
+      <h2 className="text-xl font-semibold">404 Not Found</h2>
+      <p>
+        The page you are looking for does not exist. Please check the URL or go
+        back.
+      </p>
+      <button
+        onClick={handleGoBack}
+        className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
+      >
+        Go Back
+      </button>
     </main>
   );
 }
