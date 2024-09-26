f19341cefe3e8dd792a08afbcb3d31867ec4edd5
ColinAdo <colinadodev30@gmail.com>
2024-09-26 13:54:29 +0300

    Removed undefined

--- components/profile/ProfileHeader.tsx
+++ components/profile/ProfileHeader.tsx
@@ -1,11 +1,7 @@
 import { ChevronDown, Settings, UserPlus } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
-export default function ProfileHeader({
-  username,
-}: {
-  username: string | null;
-}) {
+export default function ProfileHeader({ username }: { username: string }) {
   return (
     <header className="fixed md:hidden bg-white top-0 flex items-center dark:bg-neutral-950 w-full z-50 border-b border-zinc-300 dark:border-neutral-700 px-3 py-1 sm:-ml-6">
       <Button size={"icon"} variant={"ghost"}>
