61632d85427e173edf8d2f0e3c3a3cadffc12af3
ColinAdo <colinadodev30@gmail.com>
2024-09-26 07:39:01 +0300

    Add mad user avatar to accept className props

--- components/common/UserAvatar.tsx
+++ components/common/UserAvatar.tsx
@@ -1,31 +1,26 @@
 import { Avatar } from "@/components/ui/avatar";
 import { Spinner } from "@/components/common";
 import Image from "next/image";
-import Link from "next/link";
 import { useRetrieveProfileQuery } from "@/redux/features/profileSlice";
 import { AvatarProps } from "@radix-ui/react-avatar";
 
 type Props = Partial<AvatarProps>;
 
-export default function UserAvatar({ ...avaterProps }: Props) {
+export default function UserAvatar({ ...avatarProps }: Props) {
   const { data: profile } = useRetrieveProfileQuery();
 
-  const profileUrl = `/dashboard/profile/${profile?.profile.username}/`;
-
   return (
-    <Link href={profileUrl}>
-      <Avatar className="relative h-6 w-6 cursor-pointer">
-        {profile?.profile.profile_picture ? (
-          <Image
-            src={profile?.profile.profile_picture}
-            fill
-            alt={`${profile?.profile.username}'s avatar`}
-            className="rounded-full object-cover"
-          />
-        ) : (
-          <Spinner sm />
-        )}
-      </Avatar>
-    </Link>
+    <Avatar className="relative h-6 w-6 cursor-pointer" {...avatarProps}>
+      {profile?.profile.profile_picture ? (
+        <Image
+          src={profile?.profile.profile_picture}
+          fill
+          alt={`${profile?.profile.username}'s avatar`}
+          className="rounded-full object-cover"
+        />
+      ) : (
+        <Spinner sm />
+      )}
+    </Avatar>
   );
 }
