17260d22ba738e8183008ebca13e68667db003f3
ColinAdo <colinadodev30@gmail.com>
2024-09-26 13:56:12 +0300

    Updated schema

--- lib/schemas.ts
+++ lib/schemas.ts
@@ -76,17 +76,3 @@ export const CommentSchema = z.object({
 export const CreateComment = CommentSchema.omit({ id: true });
 export const UpdateComment = CommentSchema;
 export const DeleteComment = CommentSchema.pick({ id: true });
-
-export const UserSchema = z.object({
-  id: z.number(),
-  username: z.string().optional(),
-  name: z.string().optional(),
-  image: z.string().optional(),
-  bio: z.string().max(150).optional(),
-  website: z.string().optional(),
-  gender: z.string().optional(),
-});
-
-export const UpdateUser = UserSchema;
-export const DeleteUser = UserSchema.pick({ id: true });
-export const FollowUser = UserSchema.pick({ id: true });
