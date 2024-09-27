3031eecca782692301f651536a0e8b4e14a5c7f0
ColinAdo <colinadodev30@gmail.com>
2024-09-27 12:37:53 +0300

    Add follow / unfollow button

---
+++ components/profile/FollowButton.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+// import { followUser } from "@/lib/actions";
+// import SubmitButton from "./SubmitButton";
+import { SubmitButton } from "@/components/post";
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import {
+  useFollowProfileMutation,
+  useRetrieveUsersProfileQuery,
+} from "@/redux/features/profileSlice";
+import { FormEvent } from "react";
+import { toast } from "sonner";
+
+interface Props {
+  profileId: number;
+  username: string;
+  isFollowing?: boolean;
+  className?: string;
+  buttonClassName?: string;
+}
+
+export default function FollowButton({
+  profileId,
+  username,
+  isFollowing,
+  className,
+  buttonClassName,
+}: Props) {
+  const [followProfile] = useFollowProfileMutation();
+  const { refetch } = useRetrieveUsersProfileQuery(username);
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      await followProfile(profileId).unwrap();
+      refetch();
+    } catch (err) {
+      toast.error("failed to follow user!");
+    }
+  };
+  return (
+    <form onSubmit={handleSubmit} className={className}>
+      <SubmitButton
+        className={buttonVariants({
+          variant: isFollowing ? "secondary" : "default",
+          className: cn("!font-bold w-full mx-1", buttonClassName),
+          size: "sm",
+        })}
+      >
+        {isFollowing ? "Unfollow" : "Follow"}
+      </SubmitButton>
+    </form>
+  );
+}
