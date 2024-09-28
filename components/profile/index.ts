0a65287deff551fe789db009bae4367b1dad88e4
ColinAdo <colinadodev30@gmail.com>
2024-09-28 16:43:32 +0300

    Add export file

--- components/profile/index.ts
+++ components/profile/index.ts
@@ -3,4 +3,6 @@ export { default as ProfileAvatar } from "./ProfileAvatar";
 export { default as FollowButton } from "./FollowButton";
 export { default as ProfileTabs } from "./ProfileTabs";
 export { default as FollowersModal } from "./FollowersModal";
+export { default as FollowingModal } from "./FollowingModal";
 export { default as Follower } from "./Follower";
+export { default as Following } from "./Following";
