d09f02f71af6a5a5ff64a86ec9093c5e847aa3b5
ColinAdo <colinadodev30@gmail.com>
2024-09-28 16:43:16 +0300

    Add following components

---
+++ components/profile/Following.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import Link from "next/link";
+import FollowButton from "./FollowButton";
+import { UserAvatar } from "@/components/common";
+import { useRetrieveUserQuery } from "@/redux/features/authApiSlice";
+import { useRetrieveUsersProfileQuery } from "@/redux/features/profileSlice";
+
+interface ProfileData {
+  id: number;
+  email: string;
+  username: string;
+  profile_picture: string;
+  profession: string;
+  github: string;
+  instagram: string;
+  linkedin: string;
+  x: string;
+}
+
+interface Props {
+  username: string;
+  usernameParam: string;
+  profileId: number;
+  profileData: ProfileData;
+}
+
+export default function Follower({
+  username,
+  usernameParam,
+  profileId,
+  profileData,
+}: Props) {
+  const { data: user } = useRetrieveUserQuery();
+
+  if (!user) return null;
+  const { data: profile } = useRetrieveUsersProfileQuery(username);
+
+  const isFollowing = profile?.following.some(
+    (following) => following.follows.id === user.id
+  );
+
+  const isCurrentUser = profile?.profile.id === user.id;
+
+  console.log("isCurrentUser, isFollowing", isCurrentUser, isFollowing);
+
+  if (!profile) return null;
+
+  return (
+    <div className="p-4 flex items-center justify-between gap-x-3">
+      <Link
+        href={`/dashboard/${usernameParam}`}
+        className="flex items-center gap-x-3"
+      >
+        <UserAvatar user={profileData} className="h-10 w-10" />
+        <p className="font-bold text-sm">{usernameParam}</p>
+      </Link>
+      {isCurrentUser && (
+        <FollowButton
+          username={username}
+          profileId={profileId}
+          isFollowing={isFollowing}
+          buttonClassName={
+            isFollowing ? "bg-neutral-700 dark:hover:bg-neutral-700/40" : ""
+          }
+        />
+      )}
+    </div>
+  );
+}
