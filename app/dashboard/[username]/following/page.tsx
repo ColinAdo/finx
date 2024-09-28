412ffbbb772eaa2a5fb013aa21072a46595da2fc
ColinAdo <colinadodev30@gmail.com>
2024-09-28 16:42:19 +0300

    Updated proper content

--- app/dashboard/[username]/following/page.tsx
+++ app/dashboard/[username]/following/page.tsx
@@ -1,3 +1,21 @@
-export default function FollowingPAge() {
-  return <h2>Followings</h2>;
+"use client";
+
+import { FollowingModal } from "@/components/profile";
+import { useRetrieveUsersProfileQuery } from "@/redux/features/profileSlice";
+
+interface Props {
+  params: {
+    username: string;
+  };
+}
+
+export default function FollowersPage({ params: { username } }: Props) {
+  const { data } = useRetrieveUsersProfileQuery(username);
+
+  const following = data?.following;
+  if (!data) {
+    return;
+  }
+
+  return <FollowingModal following={following} username={username} />;
 }
