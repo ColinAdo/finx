4e82e0331c181d244c32b17531b27e7824dfc314
ColinAdo <colinadodev30@gmail.com>
2024-09-28 15:14:13 +0300

    Add followers model

---
+++ components/profile/FollowersModal.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { useMount } from "@/hooks";
+// import { FollowerWithExtras } from "@/lib/definitions";
+import { usePathname, useRouter } from "next/navigation";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Follower } from "@/components/profile";
+
+interface Props {
+  followers: any[] | undefined;
+  username: string;
+}
+
+export default function FollowersModal({ followers, username }: Props) {
+  const mount = useMount();
+  const pathname = usePathname();
+  const router = useRouter();
+  const isFollowersPage = pathname === `/dashboard/${username}/followers`;
+
+  if (!mount) return null;
+
+  return (
+    <Dialog
+      open={isFollowersPage}
+      onOpenChange={(isOpen) => !isOpen && router.back()}
+    >
+      <DialogContent className="dialogContent">
+        <DialogHeader className="border-b border-zinc-300 dark:border-neutral-700 py-2 w-full">
+          <DialogTitle className="mx-auto font-bold text-base">
+            Followers
+          </DialogTitle>
+        </DialogHeader>
+
+        {followers?.length === 0 && (
+          <p className="p-4 text-sm font-medium">This user has no followers.</p>
+        )}
+
+        <ScrollArea className="min-h-fit max-h-[350px]">
+          {followers?.map((follower) =>
+            follower.user.username !== username ? (
+              <Follower
+                key={follower?.id}
+                profileData={follower.user}
+                username={username}
+                usernameParam={follower.user.username}
+                profileId={follower.user.id}
+              />
+            ) : null
+          )}
+        </ScrollArea>
+      </DialogContent>
+    </Dialog>
+  );
+}
