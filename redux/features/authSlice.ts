4aee43fdd61ddd6e000cd6f207862ff329d771f8
ColinAdo <colinadodev30@gmail.com>
2024-10-02 13:33:11 +0300

    Add use web sicket global states

--- redux/features/authSlice.ts
+++ redux/features/authSlice.ts
@@ -3,11 +3,15 @@ import { createSlice } from "@reduxjs/toolkit";
 interface AuthState {
   isAuthenticated: boolean;
   isLoading: boolean;
+  socketConnected: boolean;
+  messages: any[];
 }
 
 const initialState = {
   isAuthenticated: false,
   isLoading: true,
+  socketConnected: false,
+  messages: [],
 } as AuthState;
 
 const authSlice = createSlice({
@@ -19,12 +23,29 @@ const authSlice = createSlice({
     },
     logout: (state) => {
       state.isAuthenticated = false;
+      state.socketConnected = false;
     },
     finishInitialLoad: (state) => {
       state.isLoading = false;
     },
+    setSocketConnected: (state) => {
+      state.socketConnected = true;
+    },
+    setSocketDisconnected: (state) => {
+      state.socketConnected = false;
+    },
+    setMessage: (state, action) => {
+      state.messages.push(action.payload);
+    },
   },
 });
 
-export const { setAuth, logout, finishInitialLoad } = authSlice.actions;
+export const {
+  setAuth,
+  logout,
+  finishInitialLoad,
+  setSocketConnected,
+  setSocketDisconnected,
+  setMessage,
+} = authSlice.actions;
 export default authSlice.reducer;
