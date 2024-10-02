15b2b79e77455333865ef888afe6c95980b7af2f
ColinAdo <colinadodev30@gmail.com>
2024-10-02 13:33:42 +0300

    Add useWebsocket hook to test

--- app/dashboard/(home)/page.tsx
+++ app/dashboard/(home)/page.tsx
@@ -1,8 +1,13 @@
+"use client";
+
 import { Suspense } from "react";
 import { Posts } from "@/components/post";
 import { PostsSkeleton } from "@/components/common/Skeletons";
+import { useWebSocket } from "@/hooks";
 
 export default function Page() {
+  useWebSocket();
+  console.log("Page component rendered");
   return (
     <main className="flex w-full flex-grow">
       <div className="flex flex-col flex-1 gap-y-8 max-w-lg mx-auto pb-20">
